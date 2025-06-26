9ce3c22c72101aa862e14ecf89c7326681f3d2c5
James <kennyjames237@gmail.com>
2025-06-26 19:17:36 +0200

    variables

--- first.js
+++ first.js
@@ -1,1 +1,11 @@
-console.log("helloworld");
+//I am learning on youtube and i am adding code and pushing in my repo 
+//after a video i practice and push the code
+console.log("helloworld")
+//variables
+let num = 4 
+console.log(num)
+num = 5
+console.log(num + 3)
+
+let user_name = " Kenneth's Account "
+console.log(user_name)
