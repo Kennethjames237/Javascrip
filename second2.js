b3e41644cef03dc6e558ef81f061308bf222067f
James <kennyjames237@gmail.com>
2025-06-27 12:07:48 +0200

    second2js

---
+++ second2.js
@@ -0,0 +1,24 @@
+//Type conversion and type coercion
+let num = 6
+console.log(num, typeof num)
+//let say i what to store 6 as a sting
+conv = String(num)
+console.log(conv, typeof conv)
+
+let x
+console.log(x,typeof x)
+
+ x = 3
+console.log(x,typeof x)
+
+x = x + " "
+console.log(x,typeof x) ///change the type number to string
+
+x = x - 2
+console.log(x,typeof x) //change x that was type string to number after subtraction
+
+
+console.log(Boolean(7))
+
+let y = "123 kenny"
+console.log(parseInt(y), typeof y)
